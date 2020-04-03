f64111917f0d06110d8d60071278b18995d36bef
Andy Isays <andyisays@gmai.com>
2020-04-03 14:20:41 +0200

    Prueba

--- misistemaventas/src/main/java/com/andyisays/misistemaventas/controller/ProductosController.java
+++ misistemaventas/src/main/java/com/andyisays/misistemaventas/controller/ProductosController.java
@@ -26,7 +26,7 @@ import javax.validation.Valid;
 public class ProductosController {
     @Autowired
     private ProductosRepository productosRepository;
-    
+    //MIERDA
     
     @Autowired
     private ProveedoresRepository proveedoresRepository;
